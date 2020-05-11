d57e642246a5eb2af8864ec04ee31878a7661e5e
Ashu Kher <ashukher1@gmail.com>
2020-05-11 12:43:17 -0400

    creating the seperate layers for the game and the controls

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ root.title("Tic-Tac-Toe")
 root.maxsize(900,600) 
 root.config(bg="black")
 
+ui_frame = Frame(root, width=600, height=100, bg="grey")
+ui_frame.grid(row=0, column=0, padx=10, pady=5) 
+
+canvas = Canvas(root, width=600, height=480, bg="white")
+canvas.grid(row=1, column=0, padx=10, pady=5)
 
 root.mainloop() 
