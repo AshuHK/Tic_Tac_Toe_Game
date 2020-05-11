fb1e36a9ec500900e06db13dc358b1414eb73711
Ashu Kher <ashukher1@gmail.com>
2020-05-11 13:32:58 -0400

    additional comments

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import random
 
 def start_game():
     """
-    Starts the game 
+    Starts the game
     """
-    # will also
+    # will also choose how the game will be played based on the opponent choice
     pass
 
 
@@ -19,27 +19,32 @@ root.config(bg="black")
 
 opponent_choice = StringVar()
 
+# create the options menu abuve
 ui_frame = Frame(root, width=600, height=100, bg="grey")
 ui_frame.grid(row=0, column=0, padx=10, pady=5)
 
+# create the game space bwlow
 canvas = Canvas(root, width=600, height=480, bg="white")
 canvas.grid(row=1, column=0, padx=10, pady=5)
 
+# makes the start button
 Button(ui_frame, text="Start", command=start_game, bg="red").grid(
     row=0, column=1, padx=5, pady=5
 )
 
+# a label for the opponent choice
 Label(ui_frame, text="Choose your opponent", padx=5, pady=5).grid(
     row=0, column=0, padx=5, pady=5
 )
 
+# create the choice for the opponent
 opponent_menu = ttk.Combobox(
     ui_frame, textvariable=opponent_choice, values=["Human", "Computer"]
 )
-
 opponent_menu.grid(row=1, column=0, padx=5, pady=5)
 
-# set the default to the human opponenet 
+# set the default to the human opponent
 opponent_menu.current(0)
 
+# run the main loop and start the application 
 root.mainloop()
