9d3c95cce9e99e3c381e4cda606367e47e56e062
Ashu Kher <ashukher1@gmail.com>
2020-05-11 13:28:05 -0400

    added opponent choice

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ root.title("Tic-Tac-Toe")
 root.maxsize(900, 600)
 root.config(bg="black")
 
+opponent_choice = StringVar()
+
 ui_frame = Frame(root, width=600, height=100, bg="grey")
 ui_frame.grid(row=0, column=0, padx=10, pady=5)
 
@@ -24,14 +26,20 @@ canvas = Canvas(root, width=600, height=480, bg="white")
 canvas.grid(row=1, column=0, padx=10, pady=5)
 
 Button(ui_frame, text="Start", command=start_game, bg="red").grid(
-    row=0, column=0, padx=5, pady=5
+    row=0, column=1, padx=5, pady=5
 )
 
 Label(ui_frame, text="Choose your opponent", padx=5, pady=5).grid(
-    row=1, column=0, padx=5, pady=5
+    row=0, column=0, padx=5, pady=5
 )
 
-Radiobutton(ui_frame, text="Another Person").grid(row=2, column=1, padx=5, pady=5)
+opponent_menu = ttk.Combobox(
+    ui_frame, textvariable=opponent_choice, values=["Human", "Computer"]
+)
 
+opponent_menu.grid(row=1, column=0, padx=5, pady=5)
+
+# set the default to the human opponenet 
+opponent_menu.current(0)
 
 root.mainloop()
